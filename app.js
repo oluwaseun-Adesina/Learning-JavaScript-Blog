40ab9d311422ddffd04914f7a81ce2b3ffbdf8ab
Oluwaseun Adesina <oluwaseunadesina8@gmail.com>
2021-12-28 02:29:34 +0100

    Express Routes and MVC

--- app.js
+++ app.js
@@ -1,91 +1,39 @@
 const express = require("express");
-const morgan = require('morgan')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogroutes');
 
 // express app
 const app = express();
 
 //CONNECT TO MONGO DB
 const dbURI = 'mongodb+srv://oluwaseun:Pass_Word_123@cluster0.3bxcv.mongodb.net/node-js-tuts?retryWrites=true&w=majority';
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => app.listen(3000))
     .catch((err) => console.log(err));
 // register view engine
-app.set('view engine', 'ejs' );
+app.set('view engine', 'ejs');
 
 //routes
 //middleware and static file
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
- 
 
-
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     //res.send('<p> Home Page</p>');
     //res.sendFile('./views/index.html',{root:__dirname});
-   res.redirect('/blogs');
+    res.redirect('/blogs');
 });
 
-app.get('/about', (req, res)=> {  
-    res.render('about',  {title: "About"})
+app.get('/about', (req, res) => {
+    res.render('about', { title: "About" })
 });
- 
-//blog 
-app.get('/blogs', (req, res) =>{
-    Blog.find().sort({ createdAt: -1})
-        .then((result) =>{
-            res.render('index', {title: 'All Blogs', blogs: result})
-        })
-        .catch((err) =>{
-            console.log(err);
-        })
-})
 
-app.post('/blogs', (req, res)=> {
-    const blog = new Blog(req.body);
-
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs');
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
-}) 
-
-app.get('/blogs/:id', (req, res) =>{
-    const id = req.params.id;
-    Blog.findById(id)
-        .then(result => {
-            res.render('details', {blog: result, title: 'Blog Details'})
-        })
-        .catch(err => {
-            console.log(err);
-        });
-})
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({redirect: '/blogs' })
-        })
-        .catch(err => console.log(err))
-})
-app.get('/blogs/create', (req, res) => {
-    res.render('create',  {title: "Create a New Blog"});
-})
-
-// //redirect
-// app.get("/about-us", (req, res) => {
-//     res.redirect('/about');
-// });
-
+//blog routes
+app.use('/blogs', blogRoutes);
 // 404 page
 app.use((req, res) => {
     //res.status(404).sendFile("./views/404.html", {root: __dirname});
-    res.status(404).render('404',  {title: "Error 404"});
+    res.status(404).render('404', { title: "Error 404" });
 });
